69fb4188e3c14bd3336e95924c1f61e2f72df6ca
Joseph Hastings <jwhastings21@gmail.com>
2023-03-10 09:22:10 -0500

    Update JIVE_v2_dataset2.R

--- batch_effect_removal/JIVE/JIVE_v2_dataset2.R
+++ batch_effect_removal/JIVE/JIVE_v2_dataset2.R
@@ -9,6 +9,13 @@ suppressMessages({
 ###
 
 BacherTCellData <- BacherTCellData()
+table(BacherTCellData$batch)
+table(BacherTCellData$batch, BacherTCellData$new_cluster_names)
+
+BacherTCellData <- BacherTCellData[, !(BacherTCellData$new_cluster_names %in% c("Cycling", "Type-1 IFN signature"))]
+table(BacherTCellData$batch)
+table(BacherTCellData$batch, BacherTCellData$new_cluster_names)
+
 bacher_metadata <- as.data.frame(colData(BacherTCellData))
 bacher_seurat <- CreateSeuratObject(counts(BacherTCellData), meta.data = bacher_metadata)
 
@@ -34,7 +41,7 @@ features <- SelectIntegrationFeatures(object.list = bacher_seurat_batch)
 BacherTCellData <- BacherTCellData[features, ]
 
 # Subset to three batches
-# BacherTCellData <- BacherTCellData[, colData(BacherTCellData)$batch %in% c(10, 16, 17)]
+BacherTCellData <- BacherTCellData[, colData(BacherTCellData)$batch %in% c(14, 15)]
 
 # Final SCE object
 data_SCE <- BacherTCellData
@@ -46,7 +53,7 @@ table(batches)
 
 clusters <- data_SCE$new_cluster_names
 # Frequency of cell types in simulation
-table(clusters)
+table(batches, clusters)
 
 counts <- counts(data_SCE)
 
@@ -54,28 +61,37 @@ dim(counts)
 
 ### Batches
 n_batches <- length(unique(batches))
-unq_batches <- unique(batches)
+unq_batches <- sort(unique(batches))
 
 jive_data <- NULL
 all_clusters <- NULL
 
 for (i in 1:n_batches) {
-  jive_data[[paste0("Batch", i)]] <- t(counts[, batches == unq_batches[i]])
-  all_clusters[[paste0("Batch", i)]] <- clusters[batches == unq_batches[i]]
+  jive_data[[unq_batches[i]]] <- as.matrix(t(counts[, batches == unq_batches[i]]))
+  all_clusters[[unq_batches[i]]] <- clusters[batches == unq_batches[i]]
 }
 
 CORES <- parallel::detectCores()
 
-tic("JIVE v2 runtime")
-JIVE_results <- jive_v2(jive_data, rankJ = 6, rankA = rep(8, length(jive_data)), method = "given", maxiter = 5000, CORES = CORES)
-# JIVE v2 runtime: 6577.61 sec elapsed
+###
+
+tic("JIVE v2 runtime (permutation)")
+JIVE_results_perm <- jive_v2(jive_data, method = "perm", maxiter = 100000, CORES = CORES)
+# JIVE v2 runtime (permutaiton): 
 toc()
 
-saveRDS(JIVE_results, file = "data/JIVE_v2_dataset2_j6_a8.rds")
+summary(JIVE_results_perm)
+
+saveRDS(JIVE_results_perm, file = "data/JIVE_v2_dataset2_alt_perm.rds")
+
 
 ###
 
-tic("JIVE v2 runtime (BIC)")
-JIVE_results_bic <- jive_v2(jive_data, method = "bic", maxiter = 5000, CORES = CORES)
-# JIVE v2 runtime: 6577.61 sec elapsed
+tic("JIVE v2 runtime (given)")
+JIVE_results <- jive_v2(jive_data, rankJ = 5, rankA = rep(9, length(jive_data)), method = "given", maxiter = 10000, CORES = CORES)
+# JIVE v2 runtime (given): 789.33 sec elapsed
 toc()
+
+summary(JIVE_results)
+
+saveRDS(JIVE_results, file = "data/JIVE_v2_dataset2_b14_b15_j10_a20.rds")
