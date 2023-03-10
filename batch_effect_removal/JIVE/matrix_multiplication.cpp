961114ae7a9347b6d6dd5b00103943f0a0439d5e
Joseph Hastings <jwhastings21@gmail.com>
2023-03-10 09:21:22 -0500

    Update matrix_multiplication.cpp

--- batch_effect_removal/JIVE/matrix_multiplication.cpp
+++ batch_effect_removal/JIVE/matrix_multiplication.cpp
@@ -33,3 +33,24 @@ SEXP eigenMapMatMult2(const Eigen::Map<Eigen::MatrixXd> A,
   Eigen::MatrixXd C = A * B;
   return Rcpp::wrap(C);
 }
+
+// [[Rcpp::export]]
+SEXP eigenBDCSVD(const Eigen::Map<Eigen::MatrixXd> A, 
+                 int n_cores){
+  
+  Eigen::setNbThreads(n_cores);
+  Eigen::BDCSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeThinU|Eigen::ComputeThinV);
+  return Rcpp::List::create( Rcpp::Named("u")=svd.matrixU(),
+                             Rcpp::Named("d")=svd.singularValues(),
+                             Rcpp::Named("v")=svd.matrixV() );
+}
+
+// [[Rcpp::export]]
+SEXP eigenSingularValues(const Eigen::Map<Eigen::MatrixXd> A, 
+                         int n_cores){
+  
+  Eigen::setNbThreads(n_cores);
+  Eigen::MatrixXd B = A.transpose() * A;
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eigensolver(B);
+  return Rcpp::wrap(eigensolver.eigenvalues());
+}
